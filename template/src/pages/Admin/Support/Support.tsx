fba5a018a8e92bea66e7525fb8e34f270d41293a
naim0018 <mdkazinaim0018@gmail.com>
2025-12-31 19:48:56 +0600

    update

--- template/src/pages/Admin/Support/Support.tsx
+++
@@ -1,6 +0,0 @@
-const Support = () => {
-  return (
-    <div>Support</div>
-  )
-}
-export default Support
--- template/src/pages/Form.tsx
+++
@@ -1,7 +0,0 @@
-import FormExamples from "@/common/DynamicForm/FormExampleAndGuide/FormExamples";
-
-const Form = () => {
-  return <FormExamples />;
-};
-
-export default Form;
